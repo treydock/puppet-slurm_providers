1d68b02f8ab727c5d4447e26cf7e6c52cf8048e0
Trey Dockendorf <tdockendorf@osc.edu>
2019-12-26 12:17:28 -0500

    Add acceptance tests for sacctmgr not being in default PATH

---
+++ spec/acceptance/99_slurm_config_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper_acceptance'
+
+describe 'slurm_config' do
+  context 'setup' do
+    it 'removes previous install' do
+      on hosts, 'rm -f /usr/bin/sacctmgr'
+      pp = <<-EOS
+      class { '::slurm':
+        conf_dir              => '/opt/slurm/etc',
+        source_install_prefix => '/opt/slurm',
+      }
+      EOS
+      apply_manifest(pp, catch_failures: true)
+    end
+  end
+
+  context 'create basic cluster' do
+    it 'runs successfully' do
+      pp = <<-EOS
+      slurm_config { 'puppet':
+        sacctmgr_path => '/opt/slurm/bin/sacctmgr',
+      }
+      slurm_cluster { 'linux': ensure => 'present' }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe command('/opt/slurm/bin/sacctmgr list cluster format=cluster,flags,features,fedstate,federation --noheader --parsable2') do
+      its(:stdout) { is_expected.to match(%r{^linux||NA||None$}) }
+    end
+  end
+
+  context 'removes cluster' do
+    it 'runs successfully' do
+      pp = <<-EOS
+      slurm_config { 'puppet':
+        sacctmgr_path => '/opt/slurm/bin/sacctmgr',
+      }
+      slurm_cluster { 'linux': ensure => 'absent' }
+      EOS
+
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe command('/opt/slurm/bin/sacctmgr show cluster linux --noheader --parsable2') do
+      its(:stdout) { is_expected.to eq('') }
+    end
+  end
+end
